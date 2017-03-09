355af879eca62c7e0dbce19c2000b78700f36c2e
beeftosino <lestgabo@gmail.com>
2017-03-09 06:47:49 +0000

    edited config files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   # Send email in development mode?
   config.action_mailer.perform_deliveries = true
-
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
